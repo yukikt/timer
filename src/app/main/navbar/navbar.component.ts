75a5bde5122c59a508438b318e361962a2d197c1
yukikt <ent.kitamura@gmail.com>
2019-08-04 16:35:15 +0900

    navbarとsidebarのルーティングとnavbarからsidebarの呼び出しを実装

--- src/app/main/navbar/navbar.component.ts
+++ src/app/main/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  @Output() event: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openMenu() {
+    this.event.emit();    
   }
 
 }
